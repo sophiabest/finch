36c5da93eb1e191212b9a5a30fc4d54767db6d53
sophiabest <bestsophia1014@gmail.com>
2022-05-13 18:46:08 -0400

    part 5

---
+++ main_app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# import your models here
+from .models import Finch, Feeding
+
+# Register your models here
+admin.site.register(Finch)
+admin.site.register(Feeding)
